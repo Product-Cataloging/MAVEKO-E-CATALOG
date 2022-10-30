567fdc0287f0b746c226cefe6de1b7a42e99109f
nigussolomon <nigussolomon444@gmail.com>
2022-10-30 03:18:38 +0300

    created the product card component - see PC-86

---
+++ src/components/ProductCard/ProductCard.js
@@ -0,0 +1,42 @@
+import React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import "./ProductCard.css";
+
+export default function ProductCard(props) {
+  return (
+    <Card sx={{ maxWidth: 250, margin: "2%" }}>
+      <CardMedia
+        component="img"
+        height="200"
+        image="https://maveko.com/assets/images/5/Cutlery-2e84f441.webp"
+        alt={props.title}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div" id="cardTitle">
+          {props.title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary" id="cardDetail">
+          {props.detail}
+        </Typography>
+
+        <div className={props.show}>
+          <TextField
+            id="outlined-basic"
+            label="Quantity"
+            variant="outlined"
+            type="number"
+          />
+          <Button variant="outlined" id="addToCart">
+            <AddShoppingCartIcon />
+          </Button>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
