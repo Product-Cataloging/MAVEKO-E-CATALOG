277ad08604c16c94bb81fd4b71a6481b84526fb6
Abrham Alemayehu <aaabrham421@gmail.com>
2022-10-30 23:13:08 +0300

    added suppliers table and api call -see PC-93

--- src/layouts/Dashboard/Pages/Supplier.js
+++ src/layouts/Dashboard/Pages/Supplier.js
@@ -1,10 +1,41 @@
-const Supplier = (props) => {
-    const suppliers = getSuppliers();
-    console.log(suppliers);
-    return ( <div>This is Supplier</div> );
+import React, { Component } from "react";
+import { apiUrl } from "../../../environment";
+import TableComponent from "../../../components/Table/Table";
+
+class Supplier extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      suppliers: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch(`${apiUrl}/suppliers`)
+      .then((response) => response.json())
+      .then((response) => this.setState({ suppliers: response.data }));
+  }
+
+  render() {
+    const rows = this.state.suppliers;
+
+    const actions = [{ label: "Edit", name: "edit", color: 'var(--blue)' }];
+
+    const columns = [
+      { label: "Company Name", name: "company_name" },
+      { label: "Email", name: "email" },
+      { label: "Address Line", name: "address_line" },
+      { label: "Primary Phone Number", name: "primary_phone_number" },
+      { label: "Postal Code", name: "postal_code" },
+      { label: "Fax", name: "fax" },
+    ];
+
+    return (
+      <div>
+        <TableComponent rows={rows} columns={columns} actions={actions} />
+      </div>
+    );
+  }
 }
- 
-function getSuppliers(){
-    
-}
+
 export default Supplier;
