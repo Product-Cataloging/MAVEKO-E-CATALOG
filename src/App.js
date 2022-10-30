7ae51d9a8b6004c0aa13aa81294484364c89cb8b
nigussolomon <nigussolomon444@gmail.com>
2022-10-30 21:50:39 +0300

    setup router -see PC-100

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from "react-router-dom";
+import ProductsLayout from './layouts/ProductsLayout/ProductsLayout'
 
 function App() {
   return (
     <div className="App">
       <h1>MAVEKO</h1>
+      <Routes>
+        <Route path="/category/:id" element={<ProductsLayout/>}></Route>
+      </Routes>
     </div>
   );
 }
