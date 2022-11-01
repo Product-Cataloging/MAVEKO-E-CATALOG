85dc6d497033a72951e326aee172c84c42c7dda9
Eyosiyas Mekbib <eyosi9000@gmail.com>
2022-11-01 07:39:25 +0300

    router configration change - see PC-85

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { Navigate, Route, Routes } from "react-router-dom";
+import ProductsLayout from './layouts/ProductsLayout/ProductsLayout'
 import Home from './layouts/home/Home';
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Routes>
+        <Route path="/" element={<Navigate to="/home"/>}/>
+        <Route path="/home" element={<Home/>}></Route>
+        <Route path="/category/:id" element={<ProductsLayout/>}></Route>
+      </Routes>
     </div>
   );
 }
