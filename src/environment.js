ec535598927b848a763af912c92b71e5c3ab93c3
Abrham Alemayehu <aaabrham421@gmail.com>
2022-11-01 15:53:08 +0300

    added api path for users -see PC-93

--- src/environment.js
+++ src/environment.js
@@ -4,3 +4,5 @@ export const suppliers_path = '/suppliers';
 export const products_path= '/products';
 export const quotation_path = '/quotation_requests';
 export const categories_path = '/category';
+export const users_path = '/users';
+export const signup_path = '/sign_up';
