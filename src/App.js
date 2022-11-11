64aa0851e9bc3cb737020626b2775aeaddb8505e
Abrham Alemayehu <aaabrham421@gmail.com>
2022-11-11 15:37:58 +0300

    added role based routing inside App.js -see PC-93

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Admin from "./layouts/Dashboard";
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
-import 'primeicons/primeicons.css';
+import "primeicons/primeicons.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>MAVEKO</h1>
+    <div className="home">
+      {localStorage.getItem("role") === "Customer" && (
+        <div>Put components for customer side here</div>
+      )}
+
+      {(localStorage.getItem("role") === "Administrator" ||
+        localStorage.getItem("role") === "Operator") && (
+        <Routes>
+          <Route path="/" element={<Navigate to="/admin" replace />}></Route>
+          <Route path="/admin/*" element={<Admin />}></Route>
+          <Route path="*" element={<Navigate to="/admin" replace />} />
+        </Routes>
+      )}
+
+      {/* {!localStorage.getItem("role") && (
+        <Routes>
+          <Route path="*" element={<Navigate to="/signin" replace />} />
+        </Routes>
+      )} */}
     </div>
   );
 }
