147275f151c69e5558b7d1ba95f4dd802da95193
Abrham Alemayehu <aaabrham421@gmail.com>
2022-11-11 15:10:54 +0300

    changed class components to functional components -see PC-93

--- src/layouts/Dashboard/Forms/ProductForm.js
+++ src/layouts/Dashboard/Forms/ProductForm.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import { useEffect, useState } from "react";
 import "./style.css";
 import TextField from "@mui/material/TextField";
 import { Button } from "primereact/button";
@@ -7,120 +7,111 @@ import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import InputLabel from "@mui/material/InputLabel";
 
-class ProductForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      formValue: { ...props.formValue },
-      categories: [...props.categories],
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.submitForm = this.submitForm.bind(this);
-  }
+const ProductForm = (props) => {
+  const [formValue, setFormValue] = useState({});
+  const [categories, setCategories] = useState([]);
 
-  handleChange(event) {
-    this.setState((state) => ({
-      formValue: {
-        ...state.formValue,
-        [event.target.name]: event.target.value,
-      },
-    }));
-  }
+  useEffect(() => {
+    setFormValue(props.formValue);
+    setCategories(props.categories);
+  }, []);
 
-  submitForm(event) {
+  const handleChange = (event) => {
+    setFormValue({ ...formValue, [event.target.name]: event.target.value });
+  };
+
+  const submitForm = (event) => {
     event.preventDefault();
-    this.props.onSubmit(this.state.formValue);
-  }
+    props.onSubmit(formValue);
+  };
 
-  render() {
-    return (
-      <form onSubmit={this.submitForm} onReset={() => this.props.onClose()}>
-        <div className="inputContainer">
-          <TextField
-            className="inputField"
-            required
-            id="name"
-            name="name"
-            value={this.state.formValue.name}
-            label="Product Name"
-            variant="standard"
-            onChange={this.handleChange}
-          />
-        </div>
-        <div className="inputContainer">
-          <TextField
-            className="inputField"
-            id="brand"
-            name="brand"
-            value={this.state.formValue.brand}
-            label="Brand"
-            variant="standard"
-            onChange={this.handleChange}
-          />
-        </div>
-        <div className="inputContainer">
-          <TextField
-            className="inputField"
-            required
-            id="image_url"
-            name="image_url"
-            value={this.state.formValue.image_url}
-            label="Image Url"
-            variant="standard"
-            onChange={this.handleChange}
-          />
-        </div>
-        <div className="inputContainer">
-          <TextField
-            className="inputField"
-            id="description"
-            name="description"
-            multiline
-            rows={3}
-            value={this.state.formValue.description}
-            label="Description"
-            variant="standard"
-            onChange={this.handleChange}
-          />
-        </div>
-        <div className="inputContainer">
-          <FormControl required variant="standard" className="inputField">
-            <InputLabel id="categoryLabel">Product Category</InputLabel>
-            <Select
-              labelId="categoryLabel"
-              id="category_id"
-              name="category_id"
-              value={this.state.formValue.category_id}
-              onChange={this.handleChange}
-            >
-              {this.state.categories.map((category) => (
-                <MenuItem key={category.id} value={category.id}>
-                  {category.name}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-        </div>
+  return (
+    <form onSubmit={submitForm} onReset={() => props.onClose()}>
+      <div className="inputContainer">
+        <TextField
+          className="inputField"
+          required
+          id="name"
+          name="name"
+          value={formValue.name}
+          label="Product Name"
+          variant="standard"
+          onChange={handleChange}
+        />
+      </div>
+      <div className="inputContainer">
+        <TextField
+          className="inputField"
+          id="brand"
+          name="brand"
+          value={formValue.brand}
+          label="Brand"
+          variant="standard"
+          onChange={handleChange}
+        />
+      </div>
+      <div className="inputContainer">
+        <TextField
+          className="inputField"
+          required
+          id="image_url"
+          name="image_url"
+          value={formValue.image_url}
+          label="Image Url"
+          variant="standard"
+          onChange={handleChange}
+        />
+      </div>
+      <div className="inputContainer">
+        <TextField
+          className="inputField"
+          id="description"
+          name="description"
+          multiline
+          rows={3}
+          value={formValue.description}
+          label="Description"
+          variant="standard"
+          onChange={handleChange}
+        />
+      </div>
+      <div className="inputContainer">
+        <FormControl required variant="standard" className="inputField">
+          <InputLabel id="categoryLabel">Product Category</InputLabel>
+          <Select
+            labelId="categoryLabel"
+            id="category_id"
+            name="category_id"
+            value={formValue.category_id}
+            onChange={handleChange}
+          >
+            {categories.map((category) => (
+              <MenuItem key={category.id} value={category.id}>
+                {category.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
 
-        <div className="buttonsContainer">
-          <Button
-            label="Cancel"
-            icon="pi pi-times"
-            size="small"
-            type="reset"
-            className="p-button-danger p-button-raised p-button-sm"
-          />
-          &nbsp;
-          <Button
-            label="Save"
-            icon="pi pi-check"
-            type="submit"
-            className="p-button-primary p-button-raised p-button-sm"
-          />
-        </div>
-      </form>
-    );
-  }
-}
+      <div className="buttonsContainer">
+        <Button
+          label="Cancel"
+          icon="pi pi-times"
+          size="small"
+          type="reset"
+          className="p-button-danger p-button-raised p-button-sm"
+        />
+        &nbsp;
+        <Button
+          label="Save"
+          icon="pi pi-check"
+          type="submit"
+          className="p-button-primary p-button-raised p-button-sm"
+        />
+      </div>
+    </form>
+  );
+};
 
 export default ProductForm;
--- src/layouts/Dashboard/Forms/SupplierForm.js
+++ src/layouts/Dashboard/Forms/SupplierForm.js
@@ -1,118 +1,115 @@
-import React, { Component } from "react";
+import { useEffect, useState } from "react";
 import "./style.css";
 import TextField from "@mui/material/TextField";
 import { Button } from "primereact/button";
 
-class SupplierForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = props.formValue;
-    this.handleChange = this.handleChange.bind(this);
-    this.submitForm = this.submitForm.bind(this);
-  }
+const SupplierForm = (props) => {
+  const [formValue, setFormValue] = useState({});
 
-  handleChange(event) {
-    this.setState({ [event.target.name]: event.target.value });
-  }
+  useEffect(() => {
+    setFormValue(props.formValue);
+  }, []);
 
-  submitForm(event) {
+  const handleChange = (event) => {
+    setFormValue({ [event.target.name]: event.target.value });
+  };
+
+  const submitForm = (event) => {
     event.preventDefault();
-    this.props.onSubmit(this.state);
-  }
+    props.onSubmit(formValue);
+  };
 
-  render() {
-    return (
-      <form onSubmit={this.submitForm} onReset={() => this.props.onClose()}>
-        <div className="inputContainer">
-          <TextField
-            className="inputField"
-            required
-            id="company_name"
-            name="company_name"
-            value={this.state.company_name}
-            label="Company Name"
-            variant="standard"
-            onChange={this.handleChange}
-          />
-        </div>
-        <div className="inputContainer">
-          <TextField
-            className="inputField"
-            required
-            id="email"
-            name="email"
-            value={this.state.email}
-            label="Email"
-            variant="standard"
-            onChange={this.handleChange}
-          />
-        </div>
-        <div className="inputContainer">
-          <TextField
-            className="inputField"
-            required
-            id="address_line"
-            name="address_line"
-            value={this.state.address_line}
-            label="Address Line"
-            variant="standard"
-            onChange={this.handleChange}
-          />
-        </div>
-        <div className="inputContainer">
-          <TextField
-            className="inputField"
-            required
-            id="primary_phone_number"
-            name="primary_phone_number"
-            value={this.state.primary_phone_number}
-            label="Primary Phone Number"
-            variant="standard"
-            onChange={this.handleChange}
-          />
-        </div>
-        <div className="inputContainer">
-          <TextField
-            className="inputField"
-            id="postal_code"
-            name="postal_code"
-            value={this.state.postal_code}
-            label="Postal Code"
-            variant="standard"
-            onChange={this.handleChange}
-          />
-        </div>
-        <div className="inputContainer">
-          <TextField
-            className="inputField"
-            id="fax"
-            name="fax"
-            value={this.state.fax}
-            label="Fax"
-            variant="standard"
-            onChange={this.handleChange}
-          />
-        </div>
+  return (
+    <form onSubmit={submitForm} onReset={() => props.onClose()}>
+      <div className="inputContainer">
+        <TextField
+          className="inputField"
+          required
+          id="company_name"
+          name="company_name"
+          value={formValue.company_name}
+          label="Company Name"
+          variant="standard"
+          onChange={handleChange}
+        />
+      </div>
+      <div className="inputContainer">
+        <TextField
+          className="inputField"
+          required
+          id="email"
+          name="email"
+          value={formValue.email}
+          label="Email"
+          variant="standard"
+          onChange={handleChange}
+        />
+      </div>
+      <div className="inputContainer">
+        <TextField
+          className="inputField"
+          required
+          id="address_line"
+          name="address_line"
+          value={formValue.address_line}
+          label="Address Line"
+          variant="standard"
+          onChange={handleChange}
+        />
+      </div>
+      <div className="inputContainer">
+        <TextField
+          className="inputField"
+          required
+          id="primary_phone_number"
+          name="primary_phone_number"
+          value={formValue.primary_phone_number}
+          label="Primary Phone Number"
+          variant="standard"
+          onChange={handleChange}
+        />
+      </div>
+      <div className="inputContainer">
+        <TextField
+          className="inputField"
+          id="postal_code"
+          name="postal_code"
+          value={formValue.postal_code}
+          label="Postal Code"
+          variant="standard"
+          onChange={handleChange}
+        />
+      </div>
+      <div className="inputContainer">
+        <TextField
+          className="inputField"
+          id="fax"
+          name="fax"
+          value={formValue.fax}
+          label="Fax"
+          variant="standard"
+          onChange={handleChange}
+        />
+      </div>
 
-        <div className="buttonsContainer">
-          <Button
-            label="Cancel"
-            icon="pi pi-times"
-            size="small"
-            type="reset"
-            className="p-button-danger p-button-raised p-button-sm"
-          />
-          &nbsp;
-          <Button
-            label="Save"
-            icon="pi pi-check"
-            type="submit"
-            className="p-button-primary p-button-raised p-button-sm"
-          />
-        </div>
-      </form>
-    );
-  }
-}
+      <div className="buttonsContainer">
+        <Button
+          label="Cancel"
+          icon="pi pi-times"
+          size="small"
+          type="reset"
+          className="p-button-danger p-button-raised p-button-sm"
+        />
+        &nbsp;
+        <Button
+          label="Save"
+          icon="pi pi-check"
+          type="submit"
+          className="p-button-primary p-button-raised p-button-sm"
+        />
+      </div>
+    </form>
+  );
+};
 
 export default SupplierForm;
